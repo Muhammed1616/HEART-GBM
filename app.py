63a9df823366b988c5a6f3fc165ed2d65c534ed4
Muhammed Golec <61287653+Muhammed1616@users.noreply.github.com>
2022-02-28 20:52:54 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('GBM.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(float(x)) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    if(prediction == 0):
+        prediction = False
+    else:
+        prediction = True
+    #output = round(prediction[0], 2)
+
+    return jsonify(prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
